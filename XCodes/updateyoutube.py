3d71ae239eea7f7b3a1e0ff7b04336fc966ecb39
amidevous <amidevous@users.noreply.github.com>
2023-11-10 18:47:05 +0100

    Update updateyoutube.py

--- XCodes/updateyoutube.py
+++ XCodes/updateyoutube.py
@@ -4,14 +4,14 @@
 import subprocess, os, sys
 
 def updateyoutube():
-    os.system("sudo rm -f /home/xtreamcodes/iptv_xtream_codes/bin/youtube-dl /home/xtreamcodes/iptv_xtream_codes/bin/youtube /usr/bin/youtube-dl /usr/local/bin/youtube-dl")
-    os.system("sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/youtube-dl")
-    os.system("sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/bin/youtube-dl")  
-    os.system("sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /home/xtreamcodes/iptv_xtream_codes/bin/youtube")
-    os.system("sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /home/xtreamcodes/iptv_xtream_codes/bin/youtube-dl")  
-    os.system("sudo chmod a+rx /usr/bin/youtube-dl")
-    os.system("sudo chmod a+rx /home/xtreamcodes/iptv_xtream_codes/bin/youtube")  
-    os.system("sudo chmod a+rx /home/xtreamcodes/iptv_xtream_codes/bin/youtube-dl")
+    os.system("rm -f /home/xtreamcodes/iptv_xtream_codes/bin/youtube-dl /home/xtreamcodes/iptv_xtream_codes/bin/youtube /usr/bin/youtube-dl /usr/local/bin/youtube-dl")
+    os.system("wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/youtube-dl")
+    os.system("wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/bin/youtube-dl")  
+    os.system("wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /home/xtreamcodes/iptv_xtream_codes/bin/youtube")
+    os.system("wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /home/xtreamcodes/iptv_xtream_codes/bin/youtube-dl")  
+    os.system("chmod a+rx /usr/bin/youtube-dl")
+    os.system("chmod a+rx /home/xtreamcodes/iptv_xtream_codes/bin/youtube")  
+    os.system("chmod a+rx /home/xtreamcodes/iptv_xtream_codes/bin/youtube-dl")
     return True
 
 if __name__ == "__main__":
