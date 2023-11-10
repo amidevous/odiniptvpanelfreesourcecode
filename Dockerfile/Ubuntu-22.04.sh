e77bdb38ebc8a20ce0d933e9bc3c88f621bda289
amidevous <amidevous@users.noreply.github.com>
2023-11-10 16:08:11 +0100

    Update Ubuntu-22.04.sh

--- Dockerfile/Ubuntu-22.04.sh
+++ Dockerfile/Ubuntu-22.04.sh
@@ -9,3 +9,6 @@ max=99999999999999
 rnd=$((SRANDOM % ( max - min + 1 ) + min))
 docker build -t "$rnd" .
 docker run -d -p 222:22 -p 25500:25500 -p 80:80 -p 8080:8080 -p 443:443 -p 444:444 "$rnd"
+rm -f $HOME/.ssh/known_hosts
+sshpass -p Ash82qc44L6ZVv /usr/bin/ssh -o StrictHostKeyChecking=no -p 222 root@127.0.0.1 service mariadb restart
+sshpass -p Ash82qc44L6ZVv /usr/bin/ssh -o StrictHostKeyChecking=no -p 222 root@127.0.0.1 /bin/bash /home/xtreamcodes/iptv_xtream_codes/start_services.sh
