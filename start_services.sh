4adfc3a86a43e905ff3757b3b5ca24b4dac3489d
amidevous <amidevous@users.noreply.github.com>
2023-11-11 22:00:31 +0100

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -23,9 +23,6 @@ mv GeoLite2-City_*/GeoLite2-City.mmdb /home/xtreamcodes/iptv_xtream_codes/GeoLit
 geoliteversion=$(find /home/xtreamcodes/iptv_xtream_codes/ -name 'GeoLite2-City_*' | sed 's|/home/xtreamcodes/iptv_xtream_codes/GeoLite2-City_||')
 rm -rf GeoLite2-City_*
 chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb 2>/dev/null
-HOST=$(python2 /home/xtreamcodes/iptv_xtream_codes/pytools/config.py DECRYPT | grep Host | sed "s|Host:                ||g")
-PASSMYSQL=$(python2 /home/xtreamcodes/iptv_xtream_codes/pytools/config.py DECRYPT | grep Password | sed "s|Password:            ||g")
-mysql -h $HOST -u user_iptvpro -p$PASSMYSQL -P 7999 xtream_iptvpro -e "UPDATE admin_settings SET value = '$geoliteversion' WHERE admin_settings.type = 'geolite2_version'; " 2>/dev/null
 chown -R xtreamcodes:xtreamcodes /sys/class/net 2>/dev/null
 chown -R xtreamcodes:xtreamcodes /home/xtreamcodes 2>/dev/null
 sleep 4
