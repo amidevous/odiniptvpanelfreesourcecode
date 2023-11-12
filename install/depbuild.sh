c471dce030835835d25e517e1ee52bba654c0278
amidevous <amidevous@users.noreply.github.com>
2023-11-12 15:52:20 +0100

    Update depbuild.sh

--- install/depbuild.sh
+++ install/depbuild.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
+# sudo wget -O /root/depbuild.sh  https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/depbuild.sh && sudo bash /root/depbuild.sh
 if [ -f "/home/xtreamcodes/iptv_xtream_codes/php-7.4.33" ]; then
     echo "update exists."
 else
-echo -e "\nChecking that minimal requirements are ok"
 # Ensure the OS is compatible with the launcher
-if [ -f /etc/centos-release ]; then
-    inst() {
-       rpm -q "$1" &> /dev/null
-    } 
-    if (inst "centos-stream-repos"); then
-    OS="CentOS-Stream"
-    else
-    OS="CentOs"
-    fi    
+if [ -f /etc/almalinux-release ]; then
+    OS="Alma Linux"
     VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/centos-release)
-    VER=${VERFULL:0:1} # return 6, 7 or 8
+    VER=${VERFULL:0:1} # return 8
 elif [ -f /etc/fedora-release ]; then
-    inst() {
-       rpm -q "$1" &> /dev/null
-    } 
     OS="Fedora"
     VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/fedora-release)
-    VER=${VERFULL:0:2} # return 34, 35 or 36
+    VER=${VERFULL:0:2}
+elif [ -f /etc/gentoo-release ]; then
+    OS="Gentoo"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/fedora-release)
+    VER=${VERFULL:0:2}
+elif [ -f /etc/SuSE-release ]; then
+    OS="OpenSUSE"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/fedora-release)
+    VER=${VERFULL:0:3}
+elif [ -f /etc/centos-release ]; then
+    OS="CentOs"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/centos-release)
+    VER=${VERFULL:0:1} # return 8
 elif [ -f /etc/lsb-release ]; then
     OS=$(grep DISTRIB_ID /etc/lsb-release | sed 's/^.*=//')
     VER=$(grep DISTRIB_RELEASE /etc/lsb-release | sed 's/^.*=//')
 elif [ -f /etc/os-release ]; then
     OS=$(grep -w ID /etc/os-release | sed 's/^.*=//')
-    VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/' | head -n 1 | tail -n 1)
+    VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/')
  else
     OS=$(uname -s)
     VER=$(uname -r)
@@ -61,7 +63,6 @@ if [[ "$VER" = "8" && "$OS" = "CentOs" ]]; then
 	fi
 
 echo "Detected : $OS  $VER  $ARCH"
-if [[ "$OS" = "CentOs" && "$VER" = "6" && "$ARCH" == "x86_64" ||
 "$OS" = "CentOs" && "$VER" = "7" && "$ARCH" == "x86_64" ||
 "$OS" = "CentOS-Stream" && "$VER" = "8" && "$ARCH" == "x86_64" ||
 "$OS" = "CentOS-Stream" && "$VER" = "9" && "$ARCH" == "x86_64" ||
