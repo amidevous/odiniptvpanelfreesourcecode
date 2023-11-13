d7afacfcd48f558b95ec603219d48b2a2387d760
amidevous <amidevous@users.noreply.github.com>
2023-11-13 13:38:47 +0100

    Update php7.2rebuild.sh

--- install/php7.2rebuild.sh
+++ install/php7.2rebuild.sh
@@ -2,66 +2,43 @@
 # sudo apt-get update
 # sudo apt-get -y install wget
 # sudo yum -y install wget
-# sudo wget https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/php7.2rebuild.sh -O /root/php7.2rebuild.sh
-# sudo dos2unix /root/php7.2build.sh
-# sudo bash /root/php7.2build.sh
+# sudo wget https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/php7.2rebuild.sh -O /root/php7.2rebuild.sh && sudo bash /root/php7.2build.sh
 echo -e "\nChecking that minimal requirements are ok"
 # Ensure the OS is compatible with the launcher
-if [ -f /etc/centos-release ]; then
-    inst() {
-       rpm -q "$1" &> /dev/null
-    } 
-    if (inst "centos-stream-repos"); then
-    OS="CentOS-Stream"
-    else
-    OS="CentOs"
-    fi    
-    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/centos-release)
-    VER=${VERFULL:0:1} # return 6, 7 or 8
+if [ -f /etc/almalinux-release ]; then
+    OS="Alma Linux"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/almalinux-release)
+    VER=${VERFULL:0:1} # return 8
 elif [ -f /etc/fedora-release ]; then
-    inst() {
-       rpm -q "$1" &> /dev/null
-    } 
     OS="Fedora"
     VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/fedora-release)
-    VER=${VERFULL:0:2} # return 34, 35 or 36
+    VER=${VERFULL:0:2}
+elif [ -f /etc/gentoo-release ]; then
+    OS="Gentoo"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/gentoo-release)
+    VER=${VERFULL:0:2}
+elif [ -f /etc/SuSE-release ]; then
+    OS="OpenSUSE"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/SuSE-release)
+    VER=${VERFULL:0:3}
+elif [ -f /etc/centos-release ]; then
+    OS="CentOs"
+    VERFULL=$(sed 's/^.*release //;s/ (Fin.*$//' /etc/centos-release)
+    VER=${VERFULL:0:1} # return 8
+	if [[ "$VER" = "8" || "$VER" = "9" ]]; then
+		OS="CentOS-Stream"
+	fi
 elif [ -f /etc/lsb-release ]; then
     OS=$(grep DISTRIB_ID /etc/lsb-release | sed 's/^.*=//')
     VER=$(grep DISTRIB_RELEASE /etc/lsb-release | sed 's/^.*=//')
 elif [ -f /etc/os-release ]; then
     OS=$(grep -w ID /etc/os-release | sed 's/^.*=//')
-    VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/' | head -n 1 | tail -n 1)
+    VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/')
  else
     OS=$(uname -s)
     VER=$(uname -r)
 fi
 ARCH=$(uname -m)
-if [[ "$VER" = "8" && "$OS" = "CentOs" ]]; then
-	echo "Centos 8 obsolete udate to CentOS-Stream 8"
-	echo "this operation may take some time"
-	sleep 60
-	# change repository to use vault.centos.org CentOS 8 found online to vault.centos.org
-	find /etc/yum.repos.d -name '*.repo' -exec sed -i 's|mirrorlist=http://mirrorlist.centos.org|#mirrorlist=http://mirrorlist.centos.org|' {} \;
-	find /etc/yum.repos.d -name '*.repo' -exec sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|' {} \;
-	#update package list
-	dnf update -y
-	#upgrade all packages to latest CentOS 8
-	dnf upgrade -y
-	#install CentOS-Stream 8 repository
-	dnf -y install centos-release-stream --allowerasing
-	#install rpmconf
-	dnf -y install rpmconf
-	#set config file with rpmconf
-	rpmconf -a
-	# remove Centos 8 repository and set CentOS-Stream 8 repository by default
-	dnf -y swap centos-linux-repos centos-stream-repos
-	# system upgrade
-	dnf -y distro-sync
-	# ceanup old rpmconf file create
-	find / -name '*.rpmnew' -exec rm -f {} \;
-	find / -name '*.rpmsave' -exec rm -f {} \;
-	OS="CentOS-Stream"
-	fi
 
 echo "Detected : $OS  $VER  $ARCH"
 if [[ "$OS" = "CentOs" && "$VER" = "7" && "$ARCH" == "x86_64" ||
