e3d357638cfc58365364a13d24dae53277789be1
amidevous <amidevous@users.noreply.github.com>
2023-11-13 13:21:29 +0100

    Update php7.2build.sh

--- install/php7.2build.sh
+++ install/php7.2build.sh
@@ -38,29 +38,6 @@ elif [ -f /etc/os-release ]; then
 fi
 ARCH=$(uname -m)
 if [[ "$VER" = "8" && "$OS" = "CentOs" ]]; then
-	echo "Centos 8 obsolete udate to CentOS-Stream 8"
-	echo "this operation may take some time"
-	sleep 60
-	# change repository to use vault.centos.org CentOS 8 found online to vault.centos.org
-	find /etc/yum.repos.d -name '*.repo' -exec sed -i 's|mirrorlist=http://mirrorlist.centos.org|#mirrorlist=http://mirrorlist.centos.org|' {} \;
-	find /etc/yum.repos.d -name '*.repo' -exec sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|' {} \;
-	#update package list
-	dnf update -y
-	#upgrade all packages to latest CentOS 8
-	dnf upgrade -y
-	#install CentOS-Stream 8 repository
-	dnf -y install centos-release-stream --allowerasing
-	#install rpmconf
-	dnf -y install rpmconf
-	#set config file with rpmconf
-	rpmconf -a
-	# remove Centos 8 repository and set CentOS-Stream 8 repository by default
-	dnf -y swap centos-linux-repos centos-stream-repos
-	# system upgrade
-	dnf -y distro-sync
-	# ceanup old rpmconf file create
-	find / -name '*.rpmnew' -exec rm -f {} \;
-	find / -name '*.rpmsave' -exec rm -f {} \;
 	OS="CentOS-Stream"
 fi
 
