f2f6fdb19a9ad3f7ec4bb6225b6a58c6f4edd372
amidevous <amidevous@users.noreply.github.com>
2023-11-14 21:48:55 +0100

    Update build-pbuilder-and-mock-install.sh

--- install/build-pbuilder-and-mock-install.sh
+++ install/build-pbuilder-and-mock-install.sh
@@ -4,7 +4,7 @@ pbuilderinstall () {
    wget https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/pbuilder/pbuilder-$1 -O /root/pbuilder/pbuilder-$1
    wget https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/pbuilder/pbuilder-$1bin - O /usr/bin/pbuilder-$1
    chmod +x /usr/bin/pbuilder-$1
-   pbuilder-$1 create
+   pbuilder-$1 create --override-config
 }
 pbuilderinstall bionic
 pbuilderinstall bullseye
