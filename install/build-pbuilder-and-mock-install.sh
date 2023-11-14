aa1a3582b0446c0c3c4748a01caad0019f3e30b7
amidevous <amidevous@users.noreply.github.com>
2023-11-14 22:51:42 +0100

    Update build-pbuilder-and-mock-install.sh

--- install/build-pbuilder-and-mock-install.sh
+++ install/build-pbuilder-and-mock-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir -p /root/pbuilder
 pbuilderinstall () {
-   wget "https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/pbuilder/pbuilder-"$1"" -O "/root/pbuilder/pbuilder-"$1""
-   wget "https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/pbuilder/pbuilder-"$1"bin" -O "/usr/bin/pbuilder-"$1""
-   chmod +x "/usr/bin/pbuilder-"$1""
-   mkdir -p "/root/pbuilder/"$1""
-   mkdir -p "/root/pbuilder/aptcache-"$1"/"
-   "pbuilder-"$1" create --override-config"
+   wget https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/pbuilder/pbuilder-$1 -O /root/pbuilder/pbuilder-$1
+   wget https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/pbuilder/pbuilder-"$1"bin -O /usr/bin/pbuilder-$1
+   chmod +x /usr/bin/pbuilder-$1
+   mkdir -p /root/pbuilder/$1
+   mkdir -p /root/pbuilder/aptcache-$1/
+   pbuilder-$1 create --override-config
 }
 pbuilderinstall bionic
 pbuilderinstall bullseye
