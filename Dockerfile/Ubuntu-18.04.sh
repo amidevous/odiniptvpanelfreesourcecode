db55fe0291cdff9f3063c5ad68edf140a915de43
amidevous <amidevous@users.noreply.github.com>
2023-11-15 16:57:50 +0100

    Create Ubuntu-18.04.sh

---
+++ Dockerfile/Ubuntu-18.04.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker ps -aq | xargs docker stop | xargs docker rm
+docker system prune -a -f
+docker image prune -a -f
+min=999999999999
+max=99999999999999
+rnd=$((SRANDOM % ( max - min + 1 ) + min))
+docker build -t "$rnd" -f Dockerfile_Ubuntu-18.04 .
+docker build -t  .
+docker run -d -p 222:22 -v /root/Python-3.10.13:/source:rw "$rnd"
+rm -f $HOME/.ssh/known_hosts
