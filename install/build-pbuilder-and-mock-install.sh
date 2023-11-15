ecda1654caa64385cbd29f7bb54c4c4c2e0a763b
amidevous <amidevous@users.noreply.github.com>
2023-11-15 11:17:59 +0100

    Update build-pbuilder-and-mock-install.sh

--- install/build-pbuilder-and-mock-install.sh
+++ install/build-pbuilder-and-mock-install.sh
@@ -26,7 +26,30 @@ ubuntuori () {
    echo 'OTHERMIRROR="$OTHERMIRROR|deb-src http://security.ubuntu.com/ubuntu '$1'-security main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
    echo 'OTHERMIRROR="$OTHERMIRROR|deb http://archive.canonical.com/ubuntu '$1' partner"' >> $HOME/pbuilder/pbuilder-$1.conf
    echo 'OTHERMIRROR="$OTHERMIRROR|deb-src http://archive.canonical.com/ubuntu '$1' partner"' >> $HOME/pbuilder/pbuilder-$1.conf
-   pbuilder-$1 create --override-config
+   sudo pbuilder-$1 create --override-config
+}
+ubuntuarc () {
+   rm -f $HOME/pbuilder/pbuilder-$1.conf
+   cp $HOME/pbuilder/pbuilder-config.conf $HOME/pbuilder/pbuilder-$1.conf
+   sudo rm -f /usr/bin/pbuilder-$1
+   sudo cp /usr/bin/pbuilder-config /usr/bin/pbuilder-$1
+   sudo chmod +x /usr/bin/pbuilder-$1
+   mkdir -p $HOME/pbuilder/$1
+   mkdir -p $HOME/pbuilder/$1/aptcache/
+   mkdir -p $HOME/pbuilder/$1/result/
+   mkdir -p $HOME/pbuilder/$1/build/
+   mkdir -p $HOME/pbuilder/$1/nonexistent/
+   mkdir -p $HOME/pbuilder/$1/hooks/
+   sed -i 's|bionic|'$1'|g' $HOME/pbuilder/pbuilder-$1.conf
+   echo 'MIRRORSITE=https://old-releases.ubuntu.com/ubuntu' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'COMPONENTS="main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'OTHERMIRROR="deb-src https://old-releases.ubuntu.com/ubuntu/ '$1' main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'OTHERMIRROR="$OTHERMIRROR|deb https://old-releases.ubuntu.com/ubuntu '$1'-security main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'OTHERMIRROR="$OTHERMIRROR|deb-src https://old-releases.ubuntu.com/ubuntu '$1'-security main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'OTHERMIRROR="$OTHERMIRROR|deb-src https://old-releases.ubuntu.com/ubuntu/ '$1'-updates main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'OTHERMIRROR="$OTHERMIRROR|deb https://old-releases.ubuntu.com/ubuntu/ '$1'-updates main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   echo 'OTHERMIRROR="$OTHERMIRROR|deb-src https://old-releases.ubuntu.com/ubuntu '$1'-security main restricted universe multiverse"' >> $HOME/pbuilder/pbuilder-$1.conf
+   sudo pbuilder-$1 create --override-config
 }
 debianori () {
    rm -f $HOME/pbuilder/pbuilder-$1.conf
@@ -51,7 +74,7 @@ debianori () {
    echo 'OTHERMIRROR="$OTHERMIRROR|deb-src http://deb.debian.org/debian '$1'-updates main contrib non-free"' >> $HOME/pbuilder/pbuilder-$1.conf
    echo 'OTHERMIRROR="$OTHERMIRROR|deb http://deb.debian.org/debian '$1'--backports/ main contrib non-free"' >> $HOME/pbuilder/pbuilder-$1.conf
    echo 'OTHERMIRROR="$OTHERMIRROR|deb-src http://deb.debian.org/debian '$1'--backports/ main contrib non-free"' >> $HOME/pbuilder/pbuilder-$1.conf
-   pbuilder-$1 create --override-config
+   sudo pbuilder-$1 create --override-config
 }
 debianarc () {
    rm -f $HOME/pbuilder/pbuilder-$1.conf
@@ -68,13 +91,59 @@ debianarc () {
    sed -i 's|bionic|'$1'|g' $HOME/pbuilder/pbuilder-$1.conf
    echo 'MIRRORSITE=http://archive.debian.org/debian/' >> $HOME/pbuilder/pbuilder-$1.conf
    echo 'COMPONENTS="main contrib non-free"' >> $HOME/pbuilder/pbuilder-$1.conf
-   pbuilder-stretch create --override-config
+   sudo pbuilder-$1 create --override-config
 }
 echo "ok"
-ubuntuori bionic
-ubuntuori focal
+sudo rm -f /usr/share/debootstrap/scripts/noble
+sudo cp /usr/share/debootstrap/scripts/jammy /usr/share/debootstrap/scripts/noble
+ubuntuori noble
+sudo rm -f /usr/share/debootstrap/scripts/mantic
+sudo cp /usr/share/debootstrap/scripts/jammy /usr/share/debootstrap/scripts/mantic
+ubuntuori mantic
+rm -f /usr/share/debootstrap/scripts/lunar
+cp /usr/share/debootstrap/scripts/jammy /usr/share/debootstrap/scripts/lunar
+ubuntuori lunar
+rm -f /usr/share/debootstrap/scripts/kinetic
+cp /usr/share/debootstrap/scripts/jammy /usr/share/debootstrap/scripts/kinetic
+ubuntuarc kinetic
 ubuntuori jammy
+ubuntuarc impish
+ubuntuarc hirsute
+ubuntuarc groovy
+ubuntuori focal
+ubuntuarc eoan
+ubuntuarc disco
+ubuntuarc cosmic
+ubuntuori bionic
+ubuntuarc artful
+ubuntuarc zesty
+ubuntuarc yakkety
+ubuntuori xenial
+ubuntuarc wily
+ubuntuarc vivid
+ubuntuarc utopic
 ubuntuori trusty
+ubuntuarc saucy
+ubuntuarc raring
+ubuntuarc quantal
+ubuntuori precise
+ubuntuarc oneiric
+ubuntuarc natty
+ubuntuarc maverick
+ubuntuori lucid
+ubuntuarc karmic
+ubuntuarc jaunty
+ubuntuarc intrepid
+ubuntuori hardy
+ubuntuarc gutsy
+ubuntuarc feisty
+ubuntuarc edgy
+ubuntuori dapper
+ubuntuarc breezy
+ubuntuarc hoary
+ubuntuarc warty
+
+
 
 
 
