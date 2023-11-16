ca2f1c228153f0319e594a0b5c3199b803935c50
amidevous <amidevous@users.noreply.github.com>
2023-11-16 16:10:20 +0100

    Update CentOS-7.sh

--- Dockerfile/CentOS-7.sh
+++ Dockerfile/CentOS-7.sh
@@ -2,13 +2,9 @@
 docker ps -aq | xargs docker stop | xargs docker rm
 docker system prune -a -f
 docker image prune -a -f
-rm -f Dockerfile
-cp Dockerfile_CentOS-7 Dockerfile
 min=999999999999
 max=99999999999999
 rnd=$((SRANDOM % ( max - min + 1 ) + min))
-docker build -t "$rnd" .
-docker run -d -p 222:22 -p 25500:25500 -p 80:80 -p 8080:8080 -p 443:443 -p 444:444 "$rnd"
+docker build -t "$rnd" -f Dockerfile_CentOS-7 .
+docker run -d -p 222:22 "$rnd"
 rm -f $HOME/.ssh/known_hosts
-sshpass -p Ash82qc44L6ZVv /usr/bin/ssh -o StrictHostKeyChecking=no -p 222 root@127.0.0.1 service mariadb restart
-sshpass -p Ash82qc44L6ZVv /usr/bin/ssh -o StrictHostKeyChecking=no -p 222 root@127.0.0.1 /bin/bash /home/xtreamcodes/iptv_xtream_codes/start_services.sh
