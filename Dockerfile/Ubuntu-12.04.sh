9e61224af3fe8098064dd08971ea6a3cd95ea2bb
amidevous <amidevous@users.noreply.github.com>
2023-11-16 16:07:08 +0100

    Create Ubuntu-12.04.sh

---
+++ Dockerfile/Ubuntu-12.04.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker ps -aq | xargs docker stop | xargs docker rm
+docker system prune -a -f
+docker image prune -a -f
+min=999999999999
+max=99999999999999
+rnd=$((SRANDOM % ( max - min + 1 ) + min))
+docker build -t "$rnd" -f Dockerfile_Ubuntu-12.04 .
+docker run -d -p 222:22 "$rnd"
+rm -f $HOME/.ssh/known_hosts
