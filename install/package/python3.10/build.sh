c853d5915a40e1b8d9a1717b22b27b1a75c3a471
amidevous <amidevous@users.noreply.github.com>
2023-11-18 21:44:01 +0100

    Create build.sh

---
+++ install/package/python3.10/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p /source/
+rm -rf /source/*
+wget https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa/+sourcefiles/python3.10/3.10.13-1+focal1/python3.10_3.10.13.orig.tar.gz -P /source/
+tar -xvf /source/python3.10_3.10.13.orig.tar.gz -C /source/
+wget https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa/+sourcefiles/python3.10/3.10.13-1+focal1/python3.10_3.10.13-1+focal1.debian.tar.xz -P /source/
+tar -xvf /source/python3.10_3.10.13-1+focal1.debian.tar.xz -C /source/Python-3.10.13
+wget -O /source/Python-3.10.13/debian/control https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/package/python3.10/debian/control
+wget -O /source/Python-3.10.13/debian/changelog https://github.com/amidevous/odiniptvpanelfreesourcecode/raw/master/install/package/python3.10/debian/changelog
+sed -i 's|focal|'$(lsb_release -sc)'|' /source/Python-3.10.13/debian/changelog
+apt-get -y build-dep /source/Python-3.10.13
+cd /source/Python-3.10.13
+debuild
+rm -f /root/python3.10-build-Ubuntu-18.04.tar
+tar -cvf /root/python3.10-build-Ubuntu-18.04.tar /source/
