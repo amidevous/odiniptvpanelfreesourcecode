8163475c4b3019c087351b509917742412242063
amidevous <amidevous@users.noreply.github.com>
2023-11-18 22:09:25 +0100

    Update build.sh

--- install/package/python3.10/build.sh
+++ install/package/python3.10/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ -f /etc/lsb-release ]; then
+    OS=$(grep DISTRIB_ID /etc/lsb-release | sed 's/^.*=//')
+    VER=$(grep DISTRIB_RELEASE /etc/lsb-release | sed 's/^.*=//')
+elif [ -f /etc/os-release ]; then
+    OS=$(grep -w ID /etc/os-release | sed 's/^.*=//')
+    VER=$(grep VERSION_ID /etc/os-release | sed 's/^.*"\(.*\)"/\1/')
+ else
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
 wget https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa/+sourcefiles/python3.10/3.10.13-1+focal1/python3.10_3.10.13.orig.tar.gz -P /source/
 tar -xvf /source/python3.10_3.10.13.orig.tar.gz -C /source/
 wget https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa/+sourcefiles/python3.10/3.10.13-1+focal1/python3.10_3.10.13-1+focal1.debian.tar.xz -P /source/
@@ -9,5 +19,5 @@ sed -i 's|focal|'$(lsb_release -sc)'|' /source/Python-3.10.13/debian/changelog
 apt-get -y build-dep /source/Python-3.10.13
 cd /source/Python-3.10.13
 debuild
-rm -f /root/python3.10-build-Ubuntu-18.04.tar
-tar -cvf /root/python3.10-build-Ubuntu-18.04.tar /source/
+rm -f "/root/python3.10-build-"$OS"-"$VER".tar"
+tar -cvf "/root/python3.10-build-"$OS"-"$VER".tar" /source/
