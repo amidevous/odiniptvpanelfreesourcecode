974a56bf2ca14fd446f3959b4e297eaac0e878d9
amidevous <amidevous@users.noreply.github.com>
2023-11-18 17:28:01 +0100

    Update Ubuntu-20.04.sh

--- Dockerfile/Ubuntu-20.04.sh
+++ Dockerfile/Ubuntu-20.04.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+docker ps -aq | xargs docker stop | xargs docker rm
+docker system prune -a -f
+docker image prune -a -f
+min=999999999999
+max=99999999999999
+rnd=$((SRANDOM % ( max - min + 1 ) + min))
+docker build -t "$rnd" -f Dockerfile_Ubuntu-20.04 .
+docker run -d -p 222:22 -v /root/source:/source:rw "$rnd"
+rm -f $HOME/.ssh/known_hosts
